28f0ffa75ca21fca4e977fef2ddf1754112b1c7a
Anton Karpenko <karpolan@karpolan.com>
2024-02-06 01:11:54 +0100

    feat: Download page + Install + Uninstall feat: Buy/Donation page

--- src/app/(main)/download/BlockDownloadButtons.tsx
+++ src/app/(main)/download/BlockDownloadButtons.tsx
@@ -2,20 +2,28 @@
 import { useOnMobile } from '@/hooks';
 import { Stack } from '@/components';
 import DownloadButton from '@/components/DownloadButton';
+import { FunctionComponent } from 'react';
+
+interface Props {
+  variant?: 'windows' | 'mac';
+}
 
 /**
  * Renders 2 "Download" buttons in column on mobile and in row on desktop
  * @component BlockDownloadButtons
  */
-const BlockDownloadButtons = () => {
+const BlockDownloadButtons: FunctionComponent<Props> = ({ variant = 'windows' }) => {
   const onMobile = useOnMobile();
   const direction = onMobile ? 'column' : 'row';
   return (
     <Stack direction={direction} padding="1rem" gap="1rem">
-      <DownloadButton file="exe">Download EXE</DownloadButton>
-      <DownloadButton file="zip" variant="outlined">
-        Download ZIP
-      </DownloadButton>
+      {variant === 'windows' && <DownloadButton file="exe">Download EXE</DownloadButton>}
+      {variant === 'windows' && (
+        <DownloadButton file="zip" variant="outlined">
+          Download ZIP
+        </DownloadButton>
+      )}
+      {variant === 'mac' && <DownloadButton file="dmg">Download DMG</DownloadButton>}
     </Stack>
   );
 };
