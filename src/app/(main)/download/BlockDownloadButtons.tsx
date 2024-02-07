afa34bf8878b5bba2578791023803e725183e3aa
Anton Karpenko <karpolan@karpolan.com>
2024-02-07 16:22:47 +0100

    feat: Home page refactor: BlockDownloadButtons

--- src/app/(main)/download/BlockDownloadButtons.tsx
+++ src/app/(main)/download/BlockDownloadButtons.tsx
@@ -5,25 +5,25 @@ import DownloadButton from '@/components/DownloadButton';
 import { FunctionComponent } from 'react';
 
 interface Props {
-  variant?: 'windows' | 'mac';
+  variant?: 'all' | 'windows' | 'mac';
 }
 
 /**
  * Renders 2 "Download" buttons in column on mobile and in row on desktop
  * @component BlockDownloadButtons
  */
-const BlockDownloadButtons: FunctionComponent<Props> = ({ variant = 'windows' }) => {
+const BlockDownloadButtons: FunctionComponent<Props> = ({ variant = 'all' }) => {
   const onMobile = useOnMobile();
   const direction = onMobile ? 'column' : 'row';
   return (
     <Stack direction={direction} padding="1rem" gap="1rem">
-      {variant === 'windows' && <DownloadButton file="exe">Download EXE</DownloadButton>}
-      {variant === 'windows' && (
+      {['all', 'windows'].includes(variant) && <DownloadButton file="exe">Download EXE</DownloadButton>}
+      {['all', 'windows'].includes(variant) && (
         <DownloadButton file="zip" variant="outlined">
           Download ZIP
         </DownloadButton>
       )}
-      {variant === 'mac' && <DownloadButton file="dmg">Download DMG</DownloadButton>}
+      {['all', 'mac'].includes(variant) && <DownloadButton file="dmg">Download DMG</DownloadButton>}
     </Stack>
   );
 };
