6b7edae5d23f22b04029380cf272305339b1e775
Anton Karpenko <karpolan@karpolan.com>
2024-02-13 17:47:07 +0100

    fix: colors

--- src/components/config.ts
+++ src/components/config.ts
@@ -2,6 +2,8 @@
  * Components configuration
  */
 
+import { COLORS } from '../style/config';
+
 /**
  * Button component
  */
@@ -13,5 +15,5 @@ export const BUTTON_ICON_SIZE = '1.5rem';
  * Icon component
  */
 export const ICON_SIZE = '4rem';
-export const ICON_COLOR_NORMAL = 'rgb(191, 42, 29)'; // '#636A7E';
+export const ICON_COLOR_NORMAL = COLORS.primary;
 export const ICON_COLOR_INVERTED = '#FFFFFF';
--- src/style/config.ts
+++ src/style/config.ts
@@ -6,7 +6,7 @@ export const COLORS = {
   text: '#222222', // rgb(34, 34, 34)
   background: '#FFFFFF',
   headerAndFooter: '#fff5ed', // rgb(255, 245, 237)
-  primary: '#BF2A1D', // rgb(255, 85, 62)
+  primary: '#BF2A1D', // rgb(191, 42, 29)
   secondary: '#404040', // rgb(64, 64, 64)
   success: '#00FF00',
   warning: '#FFFF00',
